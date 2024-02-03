7410f554defc7610e10d5d9b8c4fb0dcd9f16aed
marcoeribes <marcoeribes@gmail.com>
2024-02-02 22:08:38 -0700

    Global User on App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
+import { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+
 import "./App.css";
 import Navbar from "./components/navbar/Navbar.tsx";
 import AboutScreen from "./pages/AboutScreen.tsx";
 import HomeScreen from "./pages/HomeScreen.tsx";
 import RecipesScreen from "./pages/Recipes/RecipesScreen.tsx";
 import RecipeScreen from "./pages/Recipe/RecipeScreen.tsx";
+import LoginScreen from "./pages/Login/Login.tsx";
 
-import { Route, Routes } from "react-router-dom";
+import { supabase } from "../supabase/client.ts";
+import { User } from "@supabase/supabase-js";
 
 function App() {
+  const [user, setUser] = useState<User | null>(null); // Update the type of user state
+
+  useEffect(() => {
+    async function fetchData() {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      console.log("user", user);
+      setUser(user); // Needs Type
+    }
+    fetchData();
+  }, []);
+
   return (
     <>
       <Navbar />
@@ -17,6 +35,7 @@ function App() {
           <Route path="/recipes" element={<RecipesScreen />} />
           <Route path="/recipe/:id" element={<RecipeScreen />} />
           <Route path="/about" element={<AboutScreen />} />
+          <Route path="/login" element={<LoginScreen />} />
         </Routes>
       </div>
     </>
