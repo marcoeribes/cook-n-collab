3e100bb5c6716faac00e3106f40fd5c30b90e288
Marco <marcoeribes@gmail.com>
2026-04-09 12:59:24 -0700

    Update footer text

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,13 +6,6 @@ export default function Footer() {
       <div style={{ display: "flex", flexDirection: "column" }}>
         <div className="icons">
           <a
-            href="https://github.com/marcoeribes/cook-n-collab"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src="/icons/github-mark.svg" alt="Github" className="icon" />
-          </a>
-          <a
             href="https://discord.gg/GmBAp7g9z4"
             target="_blank"
             rel="noopener noreferrer"
@@ -25,10 +18,7 @@ export default function Footer() {
           </a>
         </div>
         <p>
-          Cook 'n' Collab 2024 - Built by{" "}
-          <a href="https://marcoeribes.me" target="_blank" className="link">
-            Marco Eribes
-          </a>
+          Cook 'n' Collab 2026
         </p>
       </div>
     </div>
